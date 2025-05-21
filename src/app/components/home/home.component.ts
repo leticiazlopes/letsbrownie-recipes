faf86ff7c43c5f8ece12d06ccafaef64179ce0e9
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-05-21 15:35:38 -0300

    :sparkles: feat: cria componentes e home

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardsComponent } from "../cards/cards.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [CardsComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
