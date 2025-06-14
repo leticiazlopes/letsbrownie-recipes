af83810dac4c9311852a7cde72e30da427c9215d
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-06-14 15:35:28 -0300

    Add Signals a Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgIf, NgFor } from '@angular/common';
+import data from '../../../../public/assets/data.json';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
+  imports: [NgIf],
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  recipe = signal<any>(null); 
 
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const found = data.find(r => r.id === id);
+    this.recipe.set(found); 
+  }
 }
